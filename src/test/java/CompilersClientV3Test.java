4d569846e3490229210f342e6ebf34177be93832
Robert Lewoń <kunolek@gmail.com>
2016-03-21 18:57:11 +0100

    UnitTests - BeforeClass setting, new testing exceptions method

--- src/test/java/CompilersClientV3Test.java
+++ src/test/java/CompilersClientV3Test.java
@@ -16,50 +16,42 @@ import com.google.gson.JsonObject;
 public class CompilersClientV3Test 
 {
 	static Map<String, String> env;
+	static CompilersClientV3 client;
 	
 	@BeforeClass
 	static public void setUpBeforeClass()
 	{
 		env = System.getenv();
+		client = new CompilersClientV3(
+				env.get("SE_ACCESS_TOKEN_COMPILERS"), 
+				env.get("SE_ENDPOINT_COMPILERS"));
 	}
 	
 	@Test
 	public void testAutorizationFail() 
 	{
-		CompilersClientV3 client = new CompilersClientV3(
+		CompilersClientV3 badclient = new CompilersClientV3(
 				"wrong access token", 
 				env.get("SE_ENDPOINT_COMPILERS"));
 		
-		assertThat("Wrong access token should implies empty response", client.test(), is(equalTo(new JsonObject())));
+		assertThat("Wrong access token should implies empty response", badclient.test(), is(equalTo(new JsonObject())));
 	}
 
 	@Test
 	public void testAutorizationSuccess() 
 	{
-		CompilersClientV3 client = new CompilersClientV3(
-				env.get("SE_ACCESS_TOKEN_COMPILERS"), 
-				env.get("SE_ENDPOINT_COMPILERS"));
-		
 		assertThat(client.test(), not(equalTo(new JsonObject())));
 	}
 	
 	@Test
 	public void testTestMethodSuccess() 
 	{
-		CompilersClientV3 client = new CompilersClientV3(
-				env.get("SE_ACCESS_TOKEN_COMPILERS"), 
-				env.get("SE_ENDPOINT_COMPILERS"));
-		
 		assertThat(client.test().get("pi").getAsDouble(), is(3.14));
 	}
 	
 	@Test
 	public void testCompilersMethodSuccess() 
 	{
-		CompilersClientV3 client = new CompilersClientV3(
-				env.get("SE_ACCESS_TOKEN_COMPILERS"), 
-				env.get("SE_ENDPOINT_COMPILERS"));
-		
 		assertThat(client.getCompilers().get("11").getAsString().charAt(0), is('C'));
 	}
 }
--- src/test/java/ProblemsClientV3Test.java
+++ src/test/java/ProblemsClientV3Test.java
@@ -7,57 +7,48 @@ import java.util.Map;
 import javax.ws.rs.NotAuthorizedException;
 
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.SphereEngine.Api.ProblemsClientV3;
 
 public class ProblemsClientV3Test 
 {
 	static Map<String, String> env;
+	static ProblemsClientV3 client;
+	
+	@Rule public ExpectedException exception = ExpectedException.none();
 	
 	@BeforeClass
 	static public void setUpBeforeClass()
 	{
 		env = System.getenv();
+		client = new ProblemsClientV3(
+				env.get("SE_ACCESS_TOKEN_PROBLEMS"), 
+				env.get("SE_ENDPOINT_PROBLEMS"));
 	}
 	
 	@Test
 	public void testAutorizationFail() 
 	{
-		ProblemsClientV3 client = new ProblemsClientV3(
+		ProblemsClientV3 badclient = new ProblemsClientV3(
 				"wrong access token", 
 				env.get("SE_ENDPOINT_PROBLEMS"));
-		
-		try {
-			client.test();
-			assertTrue(false);
-		} catch (NotAuthorizedException e) {
-			assertTrue(true);
-		}
+		exception.expect( NotAuthorizedException.class );
+		badclient.test();
 	}
 
 	@Test
 	public void testAutorizationSuccess() 
 	{
-		ProblemsClientV3 client = new ProblemsClientV3(
-				env.get("SE_ACCESS_TOKEN_PROBLEMS"), 
-				env.get("SE_ENDPOINT_PROBLEMS"));
-		
-		try {
-			client.test();
-			assertTrue(true);
-		} catch (NotAuthorizedException e) {
-			assertFalse(true);
-		}
+		client.test();
+		assertTrue(true);
 	}
 	
 	@Test
 	public void testTestMethodSuccess() 
 	{
-		ProblemsClientV3 client = new ProblemsClientV3(
-				env.get("SE_ACCESS_TOKEN_PROBLEMS"), 
-				env.get("SE_ENDPOINT_PROBLEMS"));
-		
 		assertTrue(client.test().get("message").getAsString().length() > 0);
 	}
 }
